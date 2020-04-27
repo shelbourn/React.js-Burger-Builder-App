efc0feb9e4f5c89159b5520fb568c895e89a4300
shelbourn <matthew@shelbourn.io>
2020-04-26 18:32:34 -0700

    Switch-case for ingredients

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,3 +1,45 @@
+import React from 'react'
+import styles from './BurgerIngredients.module.css'
+
 /***
  * ? STATELESS COMPONENT RENDERING BURGER INGREDIENTS TO THE SCREEN
+ */
+
+const burgerIngredient = (props) => {
+	let ingredient = null
+
+	switch (props.type) {
+		case 'bread-bottom':
+			ingredient = <div className={styles.BreadBottom}></div>
+			break
+		case 'bread-top':
+			ingredient = (
+				<div className={styles.BreadTop}>
+					<div className={styles.Seeds1}></div>
+					<div className={styles.Seeds2}></div>
+				</div>
+			)
+			break
+		case 'meat':
+			ingredient = <div className={styles.Meat}></div>
+			break
+		case 'cheese':
+			ingredient = <div className={styles.Cheese}></div>
+			break
+		case 'lettuce':
+			ingredient = <div className={styles.Lettuce}></div>
+			break
+		case 'bacon':
+			ingredient = <div className={styles.Bacon}></div>
+			break
+		default:
+			ingredient = null
+	}
+}
+
+export default burgerIngredient
+
+/***
+ * * Props Used:
+ * * {type}
  */
