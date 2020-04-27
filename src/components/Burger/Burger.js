fcbcf20eb986712fab2156135db4096510010029
shelbourn <matthew@shelbourn.io>
2020-04-27 16:31:59 -0700

    Building out dynamic handling of burger ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,13 +7,34 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients.js'
  * ? TO THE SCREEN
  */
 
+//* Object.keys() is a built-in method that takes an object
+//* and returns an array of the keys in a key/value pair
+
+//% .array() method returns a created array of the argument length
+//% Below: [...Array(props.ingredient[ingredKey])] returns a new array
+//% (with the spread operator) with a length (number of array elements)
+//% of the number of keys (number of cheeses, number of meats, etc) within
+//% each ingredient provided by props.ingredient
+
+//! Below: The returned key {ingredKey + i} creates a unique key
+//! and it MUST be identical to the keys used for all other burger building
+//! components
+
+//* Each <BurgerIngredient> must have a unique key so that it can be
+//* individually referenced in other code
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients).map(
+		(ingredKey) => {
+			return [...Array(props.ingredients[ingredKey])].map((_, i) => {
+				return <BurgerIngredient key={ingredKey + i} type={ingredKey} />
+			})
+		}
+	)
+
 	return (
 		<div className={styles.Burger}>
 			<BurgerIngredient type="bread-top" />
-			<BurgerIngredient type="lettuce" />
-			<BurgerIngredient type="cheese" />
-			<BurgerIngredient type="meat" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	)
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,10 +7,19 @@ import Burger from '../../components/Burger/Burger'
  */
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			lettuce: 1,
+			bacon: 1,
+			cheese: 2,
+			meat: 2,
+		},
+	}
+
 	render() {
 		return (
 			<Aux>
-				<Burger />
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Controls (Placeholder)</div>
 			</Aux>
 		)
