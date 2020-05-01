369b1029fa85613d3c2e8ba2e63e5e31e38a1cb5
shelbourn <matthew@shelbourn.io>
2020-05-01 13:32:17 -0700

    Restructuring file tree

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const menuButton = (props) => (
+const drawerToggle = (props) => (
 	<button onClick={props.clicked}>{props.children}</button>
 )
 
-export default menuButton
+export default drawerToggle
