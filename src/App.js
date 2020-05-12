7f1bafca3b23d1fe3a96b6678dca1e8d87f719a7
shelbourn <matthew@shelbourn.io>
2020-05-12 00:12:57 -0700

    Enabling routing.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
+import { Switch, Route } from 'react-router'
 
 //? Components in the 'components' folder are 'dumb' or stateless components
 //* Components in the 'containers' folder are 'smart' or stateful components
@@ -11,8 +12,10 @@ class App extends Component {
 		return (
 			<div>
 				<Layout>
-					<BurgerBuilder />
-					<Checkout />
+					<Switch>
+						<Route path="/checkout" component={Checkout} />
+						<Route exact path="/" component={BurgerBuilder} />
+					</Switch>
 				</Layout>
 			</div>
 		)
