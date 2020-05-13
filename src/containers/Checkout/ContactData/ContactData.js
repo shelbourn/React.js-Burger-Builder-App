912e3e1b0917967079f705749423ffbf4206a82d
shelbourn <matthew@shelbourn.io>
2020-05-13 15:46:36 -0700

    Checkout summary with form element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
-
+import styles from './ContactData.module.css'
 class ContactData extends Component {
 	state = {
 		name: '',
@@ -13,15 +13,15 @@ class ContactData extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className={styles.ContactData}>
 				<h4>Enter Your Contact Data</h4>
 				<form>
 					<input type="text" name="name" placeholder="Your Name" />
 					<input type="email" name="email" placeholder="Your Email" />
 					<input type="text" name="street" placeholder="Street" />
 					<input type="text" name="zipcode" placeholder="Zip Code" />
-					<Button btnType="Success">ORDER</Button>
 				</form>
+				<Button btnType="Success">ORDER</Button>
 			</div>
 		)
 	}
