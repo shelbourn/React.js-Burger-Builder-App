7999375aa3793ced87e68f219c471c334ba25bad
shelbourn <matthew@shelbourn.io>
2020-05-18 14:47:36 -0700

    Updating links and routes. Exact props

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -10,10 +10,10 @@ import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = (props) => (
 	<ul className={styles.NavigationItems}>
-		<NavigationItem link="/" active>
+		<NavigationItem link="/" exactProp="exact">
 			Burger Builder
 		</NavigationItem>
-		<NavigationItem link="/">Checkout</NavigationItem>
+		<NavigationItem link="/orders">Orders</NavigationItem>
 	</ul>
 )
 
