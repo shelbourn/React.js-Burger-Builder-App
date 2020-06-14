41922834483e92b03f09c57953ec8dfaa0c8e7ed
shelbourn <matthew@shelbourn.io>
2020-06-14 16:16:30 -0700

    Leaning code in orders reducer with utility func

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
 	orders: [],
@@ -6,18 +7,13 @@ const initialState = {
 	purchased: false,
 }
 
+// Leaning code using utility.js function
 const orderReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.PURCHASE_INIT:
-			return {
-				...state,
-				purchased: false,
-			}
+			return updateObject(state, { purchase: false })
 		case actionTypes.PURCHASE_BURGER_START: {
-			return {
-				...state,
-				loading: true,
-			}
+			return updateObject(state, { loading: true })
 		}
 
 		case actionTypes.PURCHASE_BURGER_SUCCESS: {
@@ -25,35 +21,26 @@ const orderReducer = (state = initialState, action) => {
 				...action.payload.orderData,
 				id: action.payload.orderId,
 			}
-			return {
-				...state,
+			return updateObject(state, {
 				loading: false,
 				purchased: true,
 				orders: state.orders.concat(newOrder),
-			}
+			})
 		}
 		case actionTypes.PURCHASE_BURGER_FAIL: {
-			return {
-				...state,
-				loading: false,
-			}
+			return updateObject(state, { loading: false })
 		}
-		case actionTypes.FETCH_ORDERS_START:
-			return {
-				...state,
-				loading: true,
-			}
-		case actionTypes.FETCH_ORDERS_SUCCESS:
-			return {
-				...state,
+		case actionTypes.FETCH_ORDERS_START: {
+			return updateObject(state, { loading: true })
+		}
+		case actionTypes.FETCH_ORDERS_SUCCESS: {
+			return updateObject(state, {
 				orders: action.payload.orders,
 				loading: false,
-			}
+			})
+		}
 		case actionTypes.FETCH_ORDERS_FAIL:
-			return {
-				...state,
-				loading: false,
-			}
+			return updateObject(state, { loading: false })
 
 		default:
 			return state
